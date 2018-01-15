f24c1171da78822466b0bfe552d7e2ae66cf6314
R-J <robin.jurinka@gmx.net>
2018-01-15 22:44:45 +0100

    Umformatieren auf Vanilla 2.5 Coding Standards und Löschen einiger unnützer Variablen

--- class.fahrzeugtermine.plugin.php
+++ class.fahrzeugtermine.plugin.php
@@ -2,85 +2,108 @@
 
 class FahrzeugTerminePlugin extends Gdn_Plugin {
 
-    public function Base_Render_Before($Sender) {
-        $Session = Gdn::Session();
-
-        if (($Sender->Menu) &&
-                ((CheckPermission('Plugins.FahrzeugTermine.View')) ||
-                (CheckPermission('Plugins.FahrzeugTermine.Freigabe')))) {
-                 $Sender->Menu->AddLink('Fahrzeug-Termine', T('Fahrzeug-Termine'), 'fahrzeug-termine');
+    public function base_render_before($sender) {
+        if (
+            ($sender->Menu) &&
+            (
+                checkPermission('Plugins.FahrzeugTermine.View') ||
+                checkPermission('Plugins.FahrzeugTermine.Freigabe')
+            )
+        ) {
+            $sender->Menu->addLink(
+                'Fahrzeug-Termine',
+                t('Fahrzeug-Termine'),
+                'fahrzeug-termine'
+            );
         }
     }
-    public function SettingsController_FahrzeugTermine_Create($Sender) {
-        $Session = Gdn::Session();
-        $Sender->Title('Fahrzeug Termine');
-        $Sender->AddSideMenu('plugin/FahrzeugTermine');
-        $Sender->Permission('Garden.Settings.Manage');
-        $Sender->Form = new Gdn_Form();
-        $Validation = new Gdn_Validation();
-        $ConfigurationModel = new Gdn_ConfigurationModel($Validation);
-        $ConfigurationModel->SetField(array(
+
+    public function settingsController_fahrzeugTermine_create($sender) {
+        $sender->title('Fahrzeug Termine');
+        $sender->addSideMenu('plugin/fahrzeugtermine');
+        $sender->permission('Garden.Settings.Manage');
+        $sender->Form = new Gdn_Form();
+        $validation = new Gdn_Validation();
+        $configurationModel = new Gdn_ConfigurationModel($validation);
+        $configurationModel->setField([
             'Plugins.FahrzeugTermine.DCount'
-        ));
-        $Sender->Form->SetModel($ConfigurationModel);
-        if ($Sender->Form->AuthenticatedPostBack() === FALSE) {
-            $Sender->Form->SetData($ConfigurationModel->Data);
+        ]);
+        $sender->Form->setModel($configurationModel);
+        if ($sender->Form->authenticatedPostBack() === FALSE) {
+            $sender->Form->setData($configurationModel->Data);
         } else {
-            $Data = $Sender->Form->FormValues();
-            if ($Sender->Form->Save() !== FALSE)
-                $Sender->StatusMessage = T("Your settings have been saved.");
+            $data = $sender->Form->formValues();
+            if ($sender->Form->save() !== FALSE) {
+                $sender->informMessage(t("Your settings have been saved."));
+            }
         }
-        //$Sender->Render($this->GetView('ft-settings.php'));
+        //$sender->Render($this->GetView('ft-settings.php'));
     }
-    public function PluginController_FahrzeugTermine_Create($Sender) {
-        $Session = Gdn::Session();
 
-        if (($Sender->Menu) && ((CheckPermission('Plugins.FahrzeugTermine.View')) ||  (CheckPermission('Plugins.FahrzeugTermine.Freigabe')))) {
-           $Sender->ClearCssFiles();
-           $Sender->AddCssFile('style.css');
-		   $Sender->AddCSSFile('fz.css', 'plugins/FahrzeugTermine');
-		   $Sender->AddJsFile('fahrzeugtermine.js', 'plugins/FahrzeugTermine');
-           $Sender->MasterView = 'default';
-           $Sender->Render('fztable', '', 'plugins/FahrzeugTermine');
-		   
-        }else echo Wrap(Anchor(Img('/plugins/FahrzeugTermine/design/AccessDenied.png',array('width'=>'100%'), array('title' => T('You Have No Permission To View This Page Go Back'))), '/discussions',array('target' => '_self')), 'h1');
+    public function pluginController_fahrzeugTermine_create($sender) {
+        if (
+              ($sender->Menu) && (
+                  checkPermission('Plugins.FahrzeugTermine.View') ||
+                  checkPermission('Plugins.FahrzeugTermine.Freigabe')
+              )
+        ) {
+            $sender->clearCssFiles();
+            $sender->addCssFile('style.css');
+            $sender->addCSSFile('fz.css', 'plugins/FahrzeugTermine');
+            $sender->addJsFile('fahrzeugtermine.js', 'plugins/FahrzeugTermine');
+            $sender->MasterView = 'default';
+            $sender->render('fztable', '', 'plugins/FahrzeugTermine');
+        } else {
+            echo wrap(
+                anchor(
+                    img(
+                        '/plugins/FahrzeugTermine/design/AccessDenied.png',
+                        array('width'=>'100%'),
+                        array('title' => t('You Have No Permission To View This Page Go Back'))
+                    ),
+                    '/discussions',
+                    array('target' => '_self')
+                ),
+                'h1'
+            );
+        }
     }
-    public function OnDisable() {
-	        $matchroute = '^fahrzeug-termine(/.*)?$';
-	        Gdn::Router()-> DeleteRoute($matchroute); 
-			$PermissionModel = Gdn::PermissionModel();
-			$PermissionModel->Undefine(
-              array(
-                  'Plugins.FahrzeugTermine.Add',
-                  'Plugins.FahrzeugTermine.View',
-				  'Plugins.FahrzeugTermine.Edit',
-                  'Plugins.FahrzeugTermine.Freigabe',
-                  'Plugins.FahrzeugTermine.Delete'
-      ));
-	}
-    public function Setup() {
-             $matchroute = '^fahrzeug-termine(/.*)?$';
-             $target = 'plugin/FahrzeugTermine$1';
-             if(!Gdn::Router()->MatchRoute($matchroute))
-				Gdn::Router()->SetRoute($matchroute,$target,'Internal'); 
-     // Set up the db structure
-    $this->Structure();
+
+    public function onDisable() {
+        $matchroute = '^fahrzeug-termine(/.*)?$';
+        Gdn::router()-> deleteRoute($matchroute); 
+        Gdn::permissionModel()->undefine([
+            'Plugins.FahrzeugTermine.Add',
+            'Plugins.FahrzeugTermine.View',
+            'Plugins.FahrzeugTermine.Edit',
+            'Plugins.FahrzeugTermine.Freigabe',
+            'Plugins.FahrzeugTermine.Delete'
+        ]);
     }
-	/**
-   * Setup database structure for model
-   */
-  public function Structure() {
-    $Database = Gdn::Database();
-    $Construct = $Database->Structure();
-    $Construct->Table('FahrzeugTermine');
-    $Construct
-            ->PrimaryKey('TerminID')
-            ->Column('Fahrzeug', 'varchar(140)')
-			->Column('Titel', 'varchar(140)')
-			->Column('Von', 'datetime')
-			->Column('Bis', 'datetime')
-            ->Column('Verantwortlich', 'varchar(140)')
-			->Column('Freischaltung', 'varchar(140)')
-            ->Set();
-  }
+
+    public function setup() {
+        $matchroute = '^fahrzeug-termine(/.*)?$';
+        $target = 'plugin/FahrzeugTermine$1';
+        if (!Gdn::router()->matchRoute($matchroute)) {
+            Gdn::router()->setRoute($matchroute,$target,'Internal'); 
+        }
+        // Set up the db structure
+        $this->structure();
+    }
+
+    /**
+     * Setup database structure for model
+     */
+    public function structure() {
+        Gdn::structure()
+            ->table('FahrzeugTermine');
+            ->primaryKey('TerminID')
+            ->column('Fahrzeug', 'varchar(140)')
+            ->column('Titel', 'varchar(140)')
+            ->column('Von', 'datetime')
+            ->column('Bis', 'datetime')
+            ->column('Verantwortlich', 'varchar(140)')
+            ->column('Freischaltung', 'varchar(140)')
+            ->set();
+    }
 }
