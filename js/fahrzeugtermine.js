27f9d8b9696b2a937a41f5ed9e69ace8ff37eed3
theknigge <tim@knigge-ronnenberg.de>
2018-01-15 18:02:31 +0100

    Update fahrzeugtermine.js
    code formatting

--- js/fahrzeugtermine.js
+++ js/fahrzeugtermine.js
@@ -1,12 +1,11 @@
-$(document).ready(function() {
-  // Show custom popups.
-  $( '.Popup' ).on( 'click', function() {
-    var infoContainer = document.getElementById( this.getAttribute( 'href' ).substr(1) );
-    var html = infoContainer.innerHTML;
-    $.popup (
-      {},
-      html
-    );
-  });
-
+$(document).ready(function() {
+  // Show custom popups.
+  $( '.Popup' ).on( 'click', function() {
+    var infoContainer = document.getElementById( this.getAttribute( 'href' ).substr(1) );
+    var html = infoContainer.innerHTML;
+    $.popup (
+      {},
+      html
+    );
+  });
 });
